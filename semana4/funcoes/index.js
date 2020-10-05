d276f1fcce35bacd931602f50c0093fad58a7dfd
Pablo <pma765@hotmail.com>
2020-10-05 16:20:25 -0300

    exercicio de funcoes

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,24 @@
+//exercicios
+//1-
+
+//a-
+//aparecerá no console:
+//10
+//50
+
+//b-
+//a função será invopcada e o calculo feito, mas nao aparecerá no console
+
+//2-
+
+//a-
+//sai no console: Darvas, Caio
+
+//b-
+//caso fosse mudado sairia no console: Amanda, Caio
+
+
+//3-
+//
+//a-
+//o codigo verificará se os numeros do primeiro array são pares, caso sejam os multiplicará por eles mesmos e adicionará o resultado em um array final
